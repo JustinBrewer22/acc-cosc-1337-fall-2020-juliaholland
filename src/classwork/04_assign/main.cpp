4ea99cc0bab2b0903ccc76704c89902e6ecc27e1
juliaholland <julia.holland2@g.austincc.edu>
2020-09-30 22:06:11 -0500

    Homework_3

--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,8 @@
 //write includes statements
-
+#include<iostream>
+#include "loops.h"
 //write using statements for cin and cout
-
+using std::cout; using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -9,7 +10,23 @@ a number, call the factorial function, and display the number's
 factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
-{
+{	
+	int num;
+	char choice;
+	do {
+		do {
+			cout<<"Enter your number: ";
+			cin>>num;
 
+		} while(num<0 || num >10);
+
+		int result = factorial(num);
+
+		cout<<"Factorial of num is: "<<result<<"\n";
+
+		cout<<"Do you want to continue(y/Y): ";
+		cin>>choice; 
+	} while (choice == 'y' || choice == 'Y');
+	
 	return 0;
 }
